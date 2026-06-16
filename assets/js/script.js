2e6bd55977a7aa0386d73ff8dfb9919a3996aded
rrana5106 <rrana5106@hotmail.com>
2026-06-16 21:23:05 +0100

    updated the api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
+
+
 function sendFetchRequest() {
   console.log("i have been click");
   fetch("https://api.restcountries.com/countries/v5?q=canada", {
     headers: {
-      Authorization: "Bearer rc_live_9c23ef7629df4055a2c031dcdc3b9f39",
-    },
+      Authorization: "Bearer rc_live_9c23ef7629df4055a2c031dcdc3b9f39"
+    }
   })
     .then(function (response) {
       return response.json();
